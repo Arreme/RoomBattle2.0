55a13ffb53ed61cb0b97444d77ed161e86bafe32
Arreme <a.reme@hotmail.com>
2021-05-24 18:17:31 +0200

    CustomizationDonete

--- Assets/Scripts/Input/UI/AssetsLoader.cs
+++ Assets/Scripts/Input/UI/AssetsLoader.cs
@@ -1,37 +1,60 @@
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
 public class AssetsLoader : MonoBehaviour
 {
     public static AssetsLoader Instance;
+
+    [SerializeField]
+    private Material baseRoomba;
+    [SerializeField]
+    private Material baseBalloon;
+
+    [SerializeField]
+    private Texture[] roombaTextures;
+    [SerializeField]
+    private Color[] roombaColors;
     public Material[] colorGetter(Colors color)
     {
         Material[] mat = new Material[2];
         switch (color)
         {
             case Colors.Blue:
-                mat[0] = (Material)AssetDatabase.LoadAssetAtPath("Assets/Art/Materials/roomba/blue/Blue.mat", typeof(Material));
-                mat[1] = (Material)AssetDatabase.LoadAssetAtPath("Assets/Art/Materials/roomba/blue/BlueBalloon.mat", typeof(Material));
+                mat[0] = new Material(baseRoomba);
+                mat[0].mainTexture = roombaTextures[0];
+                mat[1] = new Material(baseBalloon);
+                mat[1].color = roombaColors[0];
                 break;
             case Colors.Red:
-                mat[0] = (Material)AssetDatabase.LoadAssetAtPath("Assets/Art/Materials/roomba/red/Red.mat", typeof(Material));
-                mat[1] = (Material)AssetDatabase.LoadAssetAtPath("Assets/Art/Materials/roomba/red/RedBalloon.mat", typeof(Material));
+                mat[0] = new Material(baseRoomba);
+                mat[0].mainTexture = roombaTextures[1];
+                mat[1] = new Material(baseBalloon);
+                mat[1].color = roombaColors[1];
                 break;
             case Colors.Yellow:
-                mat[0] = (Material)AssetDatabase.LoadAssetAtPath("Assets/Art/Materials/roomba/yellow/Yellow.mat", typeof(Material));
-                mat[1] = (Material)AssetDatabase.LoadAssetAtPath("Assets/Art/Materials/roomba/yellow/YellowBalloon.mat", typeof(Material));
+                mat[0] = new Material(baseRoomba);
+                mat[0].mainTexture = roombaTextures[2];
+                mat[1] = new Material(baseBalloon);
+                mat[1].color = roombaColors[2];
                 break;
             case Colors.Green:
-                mat[0] = (Material)AssetDatabase.LoadAssetAtPath("Assets/Art/Materials/roomba/green/Green.mat", typeof(Material));
-                mat[1] = (Material)AssetDatabase.LoadAssetAtPath("Assets/Art/Materials/roomba/green/GreenBalloon.mat", typeof(Material));
+                mat[0] = new Material(baseRoomba);
+                mat[0].mainTexture = roombaTextures[3];
+                mat[1] = new Material(baseBalloon);
+                mat[1].color = roombaColors[3];
                 break;
             case Colors.Purple:
-                mat[0] = (Material)AssetDatabase.LoadAssetAtPath("Assets/Art/Materials/roomba/purple/Purple.mat", typeof(Material));
-                mat[1] = (Material)AssetDatabase.LoadAssetAtPath("Assets/Art/Materials/roomba/purple/PurpleBalloon.mat", typeof(Material));
+                mat[0] = new Material(baseRoomba);
+                mat[0].mainTexture = roombaTextures[4];
+                mat[1] = new Material(baseBalloon);
+                mat[1].color = roombaColors[4];
                 break;
             case Colors.Orange:
-                mat[0] = (Material)AssetDatabase.LoadAssetAtPath("Assets/Art/Materials/roomba/orange/Orange.mat", typeof(Material));
-                mat[1] = (Material)AssetDatabase.LoadAssetAtPath("Assets/Art/Materials/roomba/orange/OrangeBalloon.mat", typeof(Material));
+                mat[0] = new Material(baseRoomba);
+                mat[0].mainTexture = roombaTextures[5];
+                mat[1] = new Material(baseBalloon);
+                mat[1].color = roombaColors[5];
                 break;
         }
         return mat;
