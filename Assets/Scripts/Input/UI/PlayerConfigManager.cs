92c14f0a869c6054071caafb29375025b9ed8b3c
Arreme <a.reme@hotmail.com>
2021-06-02 14:04:33 +0200

    CommitFinalMap

--- Assets/Scripts/Input/UI/PlayerConfigManager.cs
+++ Assets/Scripts/Input/UI/PlayerConfigManager.cs
@@ -33,7 +33,7 @@ public class PlayerConfigManager : MonoBehaviour
         _configs[index].IsReady = true;
         if (_configs.All(p => p.IsReady == true))
         {
-            SceneManager.LoadScene("LevelDesign");
+            SceneManager.LoadScene("FinalV3");
         }
     }
 
--- Assets/Scripts/UI/RestartGame.cs
+++ Assets/Scripts/UI/RestartGame.cs
@@ -14,6 +14,6 @@ public class RestartGame : MonoBehaviour
 
     public void Restart()
     {
-        SceneManager.LoadScene("Final");
+        SceneManager.LoadScene("FinalV3");
     }
 }
