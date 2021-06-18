3c93849de4aaa3477ff93f0f799d6773fd9571d0
Daniel de Salabert Garcia <danielgdesalabert@gmail.com>
2021-06-18 13:02:28 +0200

    deleted unnecesary code

--- Assets/soundSettings.cs
+++ Assets/soundSettings.cs
@@ -12,39 +12,20 @@ public class soundSettings : MonoBehaviour
     [SerializeField]
     private Slider VFXVolumeSlider;
 
-    //VALUES
-    //public float MasterVolumeSet;
-    //public float MusicVolumeSet;
-    //public float VFXVolumeSet;
-
     void Start()
     {
-        //MasterVolumeSet = 50f;
-        //MusicVolumeSet = 100f;
-        //VFXVolumeSet = 100f;
-
-        //Adds a listener to the main slider and invokes a method when the value changes.
-        //MasterVolumeSlider.onValueChanged.AddListener(delegate { MasterValueChangeCheck(); });
+        //Adds listeners to the sliders and invokes a method when the value changes.
         MusicVolumeSlider.onValueChanged.AddListener(delegate { MusicValueChangeCheck(); });
         VFXVolumeSlider.onValueChanged.AddListener(delegate { VFXValueChangeCheck(); });
     }
 
-    private void MasterValueChangeCheck()
-    {
-        //MasterVolumeSet = MasterVolumeSlider.value;
-    }
-
     private void MusicValueChangeCheck()
     {
-        //MusicVolumeSet = MusicVolumeSlider.value;
-        Debug.Log(MusicVolumeSlider.value);
         AudioManager.Instance.ChangeMusicVolume(MusicVolumeSlider.value);
     }
 
     private void VFXValueChangeCheck()
     {
-        //VFXVolumeSet = VFXVolumeSlider.value;
-        Debug.Log(VFXVolumeSlider.value);
         AudioManager.Instance.ChangeSFXVolume(VFXVolumeSlider.value);
     }
 }
